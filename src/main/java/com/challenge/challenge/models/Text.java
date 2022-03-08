1a41a3bf940d7e496b941ab0479f3ac9e3b9510e
alejandrodk <ottoabarriosp@hotmail.com>
2022-03-08 10:11:21 -0300

    Define text analysis resources

--- src/main/java/com/challenge/challenge/models/Text.java
+++ src/main/java/com/challenge/challenge/models/Text.java
@@ -1,13 +1,30 @@
 package com.challenge.challenge.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Map;
 
+@Getter
+@Setter
 @AllArgsConstructor
 public class Text {
-    private final String id;
+    @JsonProperty
+    private int id;
+    @JsonProperty
     private final String hash;
+    @JsonProperty
     private final int chars;
+    @JsonProperty
     private final Map<String, Integer> result;
+    @JsonProperty
+    private boolean active = true;
+
+    public Text(String hash, int chars, Map<String, Integer> result) {
+        this.hash = hash;
+        this.chars = chars;
+        this.result = result;
+    }
 }
--- src/main/java/com/challenge/challenge/services/TextService.java
+++ src/main/java/com/challenge/challenge/services/TextService.java
@@ -1,7 +1,61 @@
 package com.challenge.challenge.services;
 
+import com.challenge.challenge.dtos.CreateTextDTO;
+import com.challenge.challenge.models.Text;
+import com.challenge.challenge.utils.TextUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TextService {
+    private List<String> splitIntoSyllables(String text, int charts) {
+        if (TextUtils.charsExceedsText(text, charts)) return Collections.singletonList(text);
+        // TODO:
+
+        return Collections.emptyList();
+    }
+
+    private HashMap<String, Integer> reduceResult(List<String> syllables) {
+        // TODO:
+        return new HashMap<>();
+    }
+
+    private Text analyze(CreateTextDTO dto) {
+        String normalizedText = TextUtils.normalize(dto.getText());
+        int totalChars = TextUtils.getChars(dto);
+
+        String hash = UUID.randomUUID().toString();
+        List<String> syllables = this.splitIntoSyllables(normalizedText, totalChars);
+        HashMap<String, Integer> result = this.reduceResult(syllables);
+
+        return new Text(hash, totalChars, result);
+    }
+
+    public Text create(CreateTextDTO dto) {
+        // TODO:
+        Text text = this.analyze(dto);
+        TextUtils.validateProperties(text);
+        text.setId(1);
+
+        return text;
+    }
+
+    public Text get(String id) {
+        // TODO:
+        return new Text("", 1, new HashMap<>());
+    }
+
+    public List<Text> list() {
+        // TODO:
+        return Collections.emptyList();
+    }
+
+    public boolean delete(String id) {
+        // TODO:
+        return true;
+    }
 }
