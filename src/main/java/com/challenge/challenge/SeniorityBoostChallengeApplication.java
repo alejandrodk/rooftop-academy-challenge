5535c85391a316cbe827e388ebb32810fdc5a59f
alejandrodk <ottoabarriosp@hotmail.com>
2022-03-07 23:35:10 -0300

    initial commit

---
+++ src/main/java/com/challenge/challenge/SeniorityBoostChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorityBoostChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorityBoostChallengeApplication.class, args);
+	}
+
+}
