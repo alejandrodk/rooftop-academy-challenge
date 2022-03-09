bc856fdcfb6f7dc5244e1985021a4fd17fd23c50
alejandrodk <ottoabarriosp@hotmail.com>
2022-03-09 01:28:15 -0300

    Implement H2 database

--- src/main/java/com/challenge/challenge/SeniorityBoostChallengeApplication.java
+++ src/main/java/com/challenge/challenge/SeniorityBoostChallengeApplication.java
@@ -1,13 +1,19 @@
 package com.challenge.challenge;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SeniorityBoostChallengeApplication {
+public class SeniorityBoostChallengeApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate template;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SeniorityBoostChallengeApplication.class, args);
@@ -21,5 +27,11 @@ public class SeniorityBoostChallengeApplication {
 				registry.addMapping("*").allowedOrigins("*").allowedMethods("*");
 			}
 		};
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		template.execute("DROP TABLE TEXT IF EXISTS");
+		template.execute("CREATE TABLE TEXT(id INTEGER(11) PRIMARY KEY auto_increment,hash VARCHAR(255),chars INTEGER,result VARCHAR,active BOOLEAN)");
 	}
 }
